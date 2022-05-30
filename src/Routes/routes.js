e7b47ed41f90727112fa3a1caee6eab1858f29d6
Noe Maiquez <nmaiquez@santandertecnologia.com.ar>
2022-05-30 11:27:55 -0300

    nueva sidebar con navegacion

--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -1,21 +1,18 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom'
 
-import Home from '../pages/Home/Home';
+import Home from '../pages/Home/Home'
 import ProductsList from '../pages/Products/ProductsList/ProductsList'
 import ProductView from '../pages/ProductView/ProductView'
 
-const  MainRoutes = () => {
-
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/products" element={<ProductsList/>}/>
-                <Route path="/products/:id" element={<ProductView/>}/>
-                {/* <Route path="/Productos/new" element={<Products/>}/> */}
-            </Routes>
-        </BrowserRouter>
-    )
+const MainRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/products" element={<ProductsList />} />
+      <Route path="/products/:id" element={<ProductView />} />
+      {/* <Route path="/Productos/new" element={<Products/>}/> */}
+    </Routes>
+  )
 }
 
-export default MainRoutes;
+export default MainRoutes
