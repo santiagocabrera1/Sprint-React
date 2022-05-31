1dc1c132cab6fdbbf06950706b0f9c46d53eb2cc
Cabrera <A446462@santandertecnologia.com.ar>
2022-05-31 11:50:06 -0300

    agregue loader

--- src/pages/Products/ProductsList/ProductsList.js
+++ src/pages/Products/ProductsList/ProductsList.js
@@ -1,40 +1,39 @@
 import React, { Fragment } from 'react';
 import {useEffect,useState } from 'react';
- import {useNavigate} from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
 import useFetch from '../../../hooks/useFetch';
 import ArticleProducts from '../../../components/ArticleProducts';
+import Loader from '../../../components/Loader';
 
 
 const ProductList = () => {
 
     const navigate = useNavigate() 
 
-    
     const handleNavegation = () =>{
         navigate ('/products/:id')
     }
-
     const handleAgregarProducto = () =>{
         navigate ('.productos/new')
 
     }
+    const [loading,setLoading] = useState(false);
+
     const {producto} = useFetch()
     // const [products, setProducts] = useState([])
     // useEffect (() =>{
     //     fetch('http://localhost:3000/products')
     //     .then( response=> response.json() )
     //     .then(data => console.log(data))
-        // console.log(useFetch());
+    // console.log(useFetch());
         console.log(producto,);
     return (
-
          <Fragment>
-            {/* {console.log(products)} */}
              <div>
              {producto?.map(product => (
                     <ArticleProducts title = {product.title} id = {product.id} img ={product.image} key = {product.id}/>
                 ))}
-                {/* <button className='agregarProducto' onClick={handleAgregarProducto}></button> */}
+                {loading && <Loader/>}
             </div>
         </Fragment>
     )
