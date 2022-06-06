ea8ce0d3568cbe53e6159db36ae99dbecdf1f618
Noe Maiquez <nmaiquez@santandertecnologia.com.ar>
2022-06-06 15:47:56 -0300

    tests buttons de articleHome y componente header

--- src/__Tests__/TestButtons.test.js
+++ src/__Tests__/TestButtons.test.js
@@ -1,33 +1,30 @@
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom';
-import SideBar from '../components/Sidebar';
-import { MemoryRouter } from 'react-router-dom';
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import SideBar from '../components/Sidebar'
+import { MemoryRouter } from 'react-router-dom'
 
-describe(' SideBar' , () => {
-    test('ButtonProducts', () =>{
-        //Renderizamos el componente que queremos testear
-        //Usamos el MmemoryRouter porque dentro de sidebar se encuentran los links de navbar
-        render( <SideBar/>, { wrapper: MemoryRouter } )
-        //Nos aseguramos de que el link de productos exista y lo pasamos a una variable
-        const  button=screen.getByRole('link',{name:/productos/i})
-        //Y en la priemra asercion buscamos que button nos lleve a /products
-        expect(button).toHaveAttribute('href','/products')
-        //Y en la segunda asercion buscamos que button matchee con productos (que es el nombre del boton)
-        expect(button.innerHTML).toMatch('Productos')
-    })
+describe(' SideBar', () => {
+  test('ButtonProducts', () => {
+    //Renderizamos el componente que queremos testear
+    //Usamos el MmemoryRouter porque dentro de sidebar se encuentran los links de navbar
+    render(<SideBar />, { wrapper: MemoryRouter })
+    //Nos aseguramos de que el link de productos exista y lo pasamos a una variable
+    const button = screen.getByRole('link', { name: /productos/i })
+    //Y en la priemra asercion buscamos que button nos lleve a /products
+    expect(button).toHaveAttribute('href', '/products')
+    //Y en la segunda asercion buscamos que button matchee con productos (que es el nombre del boton)
+    expect(button.innerHTML).toMatch('Productos')
+  })
 
-    test('ButtonTienda', () =>{
-        //Renderizamos el componente que queremos testear
-        //Usamos el MmemoryRouter porque dentro de sidebar se encuentran los links de navbar
-        render( <SideBar/>, { wrapper: MemoryRouter } )
-        //Nos aseguramos de que el link de productos exista y lo pasamos a una variable
-        const  button=screen.getByRole('link',{name:/tienda/i})
-        //Y en la priemra asercion buscamos que button nos lleve a /products
-        expect(button).toHaveAttribute('href','/store')
-        //Y en la segunda asercion buscamos que button matchee con productos (que es el nombre del boton)
-        expect(button.innerHTML).toMatch('Productos')
-    })
-
+  test('ButtonTienda', () => {
+    //Renderizamos el componente que queremos testear
+    //Usamos el MmemoryRouter porque dentro de sidebar se encuentran los links de navbar
+    render(<SideBar />, { wrapper: MemoryRouter })
+    //Nos aseguramos de que el link de productos exista y lo pasamos a una variable
+    const button = screen.getByRole('link', { name: /tienda/i })
+    //Y en la priemra asercion buscamos que button nos lleve a /products
+    expect(button).toHaveAttribute('href', '/store')
+    //Y en la segunda asercion buscamos que button matchee con productos (que es el nombre del boton)
+    expect(button.innerHTML).toMatch('Tienda')
+  })
 })
-
-
---
+++ src/__Tests__/buttonArticleHome.test.js
@@ -0,0 +1,31 @@
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import { MemoryRouter } from 'react-router-dom'
+import ArticleHome from '../components/ArticleHome'
+import { faBox, faShop } from '@fortawesome/free-solid-svg-icons'
+
+describe('Componente article que se ve en la home del sitio', () => {
+  test('contiene un button-link que me lleva a la lista de productos', () => {
+    render(<ArticleHome title="Productos" url="products" cantidad={10} icon={faShop} />, {
+      wrapper: MemoryRouter,
+    })
+    const link = screen.getByRole('link', { name: /ver listado/i })
+    expect(link).toHaveAttribute('href', '/products')
+  })
+
+  test('contiene un button-link que me permite agregar un nuevo producto', () => {
+    render(<ArticleHome title="Productos" url="products" cantidad={10} icon={faBox} />, {
+      wrapper: MemoryRouter,
+    })
+    const link = screen.getByRole('link', { name: /agregar/i })
+    expect(link).toHaveAttribute('href', '/products/new')
+  })
+
+  //   test('contiene un button-link que me lleva a la lista de tiendas', () => {
+  //     render(<ArticleHome title="Tienda" url="store" cantidad={3} icon={faBox} />, {
+  //       wrapper: MemoryRouter,
+  //     })
+  //     const link = screen.getByRole('link', {name: //i})
+  //     expect(link).toHaveAttribute('href', '/store')
+  //   })
+})
